5e87edcf4b274d1338b6c92affcc6958312a233f
nue-os <kimsoeun0108@gmail.com>
2025-05-23 15:13:09 +0900

    feat: MongoDB 연결

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true, // 쿠키 전송 위함
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.MOGODB_DB_NAME,
+  })
+  .then(() => {
+    console.log("MongoDB 연결됨");
+  })
+  .catch((err) => {
+    console.log("MongoDB 연결 실패 ", err);
+  });
 
 app.listen(port, () => {
   console.log(`${port}번 포트에서 실행 중`);
